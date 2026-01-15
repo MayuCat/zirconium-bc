85f92e7a84eb84af856e8d06017d26403cb7bd94
MayuCat <iifullmetalxx@gmail.com>
2026-01-15 07:50:23 -0800

    actually pushed changes cuz im silly :3

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y install broadcom-wl
 
 # Use a COPR Example:
 #
@@ -20,5 +20,3 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
-
-systemctl enable podman.socket
